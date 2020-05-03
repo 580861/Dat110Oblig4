d3143a0b9bb8b46c2e40179cf2fae4f884859aac
Abdullahi <Abdullahi@Lefdal>
2020-05-03 17:41:50 +0200

    step 2

--- ACCloudService/src/main/java/no/hvl/dat110/ac/restservice/AccessLog.java
+++ ACCloudService/src/main/java/no/hvl/dat110/ac/restservice/AccessLog.java
@@ -6,41 +6,48 @@ import java.util.concurrent.atomic.AtomicInteger;
 import com.google.gson.Gson;
 
 public class AccessLog {
-	
+
 	// atomic integer used to obtain identifiers for each access entry
 	private AtomicInteger cid;
 	protected ConcurrentHashMap<Integer, AccessEntry> log;
-	
-	public AccessLog () {
-		this.log = new ConcurrentHashMap<Integer,AccessEntry>();
+
+	public AccessLog() {
+		this.log = new ConcurrentHashMap<Integer, AccessEntry>();
 		cid = new AtomicInteger(0);
 	}
 
-	// TODO: add an access entry to the log for the provided message and return assigned id
+	// TODO: add an access entry to the log for the provided message and return
+	// assigned id
 	public int add(String message) {
-		
-		int id = 0;
-		
+		int id = cid.getAndIncrement();
+		log.put(id, new AccessEntry(id, message));
+
 		return id;
 	}
-		
+
 	// TODO: retrieve a specific access entry from the log
 	public AccessEntry get(int id) {
-		
-		return null;
-		
+
+		AccessEntry entry = null;
+		try {
+			entry = log.get(id);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return entry;
+
 	}
-	
+
 	// TODO: clear the access entry log
 	public void clear() {
-		
+		log.clear();
 	}
-	
+
 	// TODO: return JSON representation of the access log
-	public String toJson () {
-    	
-		String json = null;
-    	
-    	return json;
-    }
+	public String toJson() {
+
+		Gson gson = new Gson();
+
+		return gson.toJson(log);
+	}
 }
--- ACCloudService/src/main/java/no/hvl/dat110/ac/restservice/App.java
+++ ACCloudService/src/main/java/no/hvl/dat110/ac/restservice/App.java
@@ -1,23 +1,23 @@
 package no.hvl.dat110.ac.restservice;
 
 import static spark.Spark.after;
+import static spark.Spark.delete;
 import static spark.Spark.get;
 import static spark.Spark.port;
-import static spark.Spark.put;
 import static spark.Spark.post;
-import static spark.Spark.delete;
-
 import com.google.gson.Gson;
+
+import static spark.Spark.put;;
 
 /**
  * Hello world!
  *
  */
 public class App {
-	
+
 	static AccessLog accesslog = null;
 	static AccessCode accesscode = null;
-	
+
 	public static void main(String[] args) {
 
 		if (args.length > 0) {
@@ -27,25 +27,69 @@ public class App {
 		}
 
 		// objects for data stored in the service
-		
+
 		accesslog = new AccessLog();
-		accesscode  = new AccessCode();
-		
+		accesscode = new AccessCode();
+
 		after((req, res) -> {
-  		  res.type("application/json");
-  		});
-		
+			res.type("application/json");
+		});
+
 		// for basic testing purposes
 		get("/accessdevice/hello", (req, res) -> {
-			
-		 	Gson gson = new Gson();
-		 	
-		 	return gson.toJson("IoT Access Control Device");
+
+			Gson gson = new Gson();
+
+			return gson.toJson("IoT Access Control Device");
 		});
-		
+
 		// TODO: implement the routes required for the access control service
 		// as per the HTTP/REST operations describined in the project description
 		
-    }
-    
+		// Post , record an access entry in the log
+		post("/accessdevice/log/", (req, res) -> {
+			Gson gson = new Gson();
+			AccessMessage msg = gson.fromJson(req.body(), AccessMessage.class);
+			int id = accesslog.add(msg.getMessage());
+
+			return gson.toJson(accesslog.get(id));
+		});
+
+		// get
+		// retrieve the access log
+		get("/accessdevice/log/", (req, res) -> accesslog.toJson());
+		//retrive a specific entry in the access log
+		get("/accessdevice/log/:id", (req, res) -> {
+			Gson gson = new Gson();
+			int id = Integer.parseInt(req.params(":id"));
+
+			return gson.toJson(accesslog.get(id));
+		});
+
+		// put 
+		// updates the access code
+		put("/accessdevice/code", (req, res) -> {
+			Gson gson = new Gson();
+			AccessCode code = gson.fromJson(req.body(), AccessCode.class);
+			accesscode.setAccesscode(code.getAccesscode());
+
+			return gson.toJson(accesscode);
+		});
+		//  return the current access code stored in the server
+		get("/accessdevice/code", (req, res) -> {
+			Gson gson = new Gson();
+
+			return gson.toJson(accesscode);
+		});
+		
+		// delete
+		//delete all log entries
+		delete("/accessdevice/log/", (req, res) -> {
+			accesslog.clear();
+
+			return accesslog.toJson();
+		});
+
+	}
+
 }
